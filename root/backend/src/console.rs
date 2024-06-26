d2f9377c35ba60531a0bc87cceda5ff5ce70f8a5
veynko <me@veynko.ru>
2024-06-26 08:48:35 +0500

    move start function to console.rs

--- root/backend/src/console.rs
+++ root/backend/src/console.rs
@@ -143,3 +143,24 @@ impl ConsoleGame {
         };
     }
 }
+
+pub fn start_client(){
+    println!("Input your player x(1) o(2)");
+
+    let mut player = String::new();
+
+    io::stdin().read_line(&mut player).expect("err reading string");
+
+    let player: u8 = player.trim().parse().expect("fuck you");
+
+    if player != 1 && player != 2 {
+        println!("fuck you");
+        return;
+    }
+
+    let settings = game::Settings::bot_game(1, 9);
+
+    let mut game = ConsoleGame::new(settings);
+
+    game.play();
+}
--- root/backend/src/main.rs
+++ root/backend/src/main.rs
@@ -1,31 +1,6 @@
 mod api;
 mod console;
-
-use std::io;
-
-use crate::api::game;
 use clap::{Parser, ValueEnum};
-
-fn start(){
-    println!("Input your player x(1) o(2)");
-
-    let mut player = String::new();
-
-    io::stdin().read_line(&mut player).expect("err reading string");
-
-    let player: u8 = player.trim().parse().expect("fuck you");
-
-    if player != 1 && player != 2 {
-        println!("fuck you");
-        return;
-    }
-
-    let settings = game::Settings::bot_game(1, 9);
-
-    let mut game = console::ConsoleGame::new(settings);
-
-    game.play();
-}
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -46,7 +21,7 @@ fn main() {
     let args = Cli::parse();
 
     match args.mode {
-        Mode::Client => {start()},
+        Mode::Client => {console::start_client()},
         Mode::Server => {println!("Not implemented")}
     }
 }
