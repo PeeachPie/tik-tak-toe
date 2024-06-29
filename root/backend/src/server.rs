33d792698efe4d12bafb53009513cd1a73e3592b
veynko <me@veynko.ru>
2024-06-29 20:11:29 +0500

    huy

--- root/backend/src/server.rs
+++ root/backend/src/server.rs
@@ -1,4 +1,4 @@
-use axum::{routing::get, Router};
+use axum::{http::StatusCode, routing::{get, post}, Json, Router};
 // use serde::{Deserialize, Serialize};
 
 #[tokio::main]
@@ -6,19 +6,27 @@ pub async fn start_server() {
     // initialize tracing
     tracing_subscriber::fmt::init();
 
-    // build our application with a route
-    let app = Router::new()
-        // `GET /` goes to `root`
-        .route("/", get(root));
-        // `POST /users` goes to `create_user`
-        // .route("/users", post(create_user));
+    let api_route = Router::new()   
+        .route("/hello_world", get(hello_world))
+        .route("get_move", post(get_best_move));
 
-    // run our app with hyper, listening globally on port 3000
+
+    let app = Router::new()
+        .fallback(fallback)
+        .nest("/api", api_route);
+
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
+async fn fallback() -> (StatusCode, &'static str) {
+    (StatusCode::NOT_FOUND, "Not found")
+}
+
 // basic handler that responds with a static string
-async fn root() -> &'static str {
+async fn hello_world() -> &'static str {
     "Hello, World!\n"
 }
+
+async fn get_best_move(Json(payload): Json<Value>) -> Json<Value> {}
+
